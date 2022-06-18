3f1099fcdee17c9b9d313fee5037c3233f00a23a
isk-bokang <bo.kang@iskra.world>
2022-06-18 12:12:31 +0900

    add ErrorBoundary Dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ErrorBoundary, FallbackProps} from 'react-error-boundary'
+
+function ErrorFallback({error , resetErrorBoundary} : FallbackProps) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
+function ErrorOccurComponent(){
+  
+//throw new Error('component error')
+
+  async function event1(){
+    console.log('event1')
+    let ret = 0;
+    for(let i = 0; i < 10; i ++)
+      ret += i
+    return ret
+  }
+  async function event2(str : number){
+    console.log('event2')
+    console.log(str)
+  }
+  async function onClickErrorOccurHandle(){
+    let data = await event1()
+    event2(data)
+  }
+  
+  return (
+    <>
+      <button onClick={()=>onClickErrorOccurHandle()}> OCCUR ERROR </button>
+    </>
+  )
+}
+
 
 function App() {
+
+  function onClickErrorHandle(){
+    throw new Error("ON CLICK ERROR")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <ErrorOccurComponent/>
+      </ErrorBoundary>
     </div>
   );
 }
